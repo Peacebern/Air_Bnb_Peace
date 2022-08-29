2e4e48a2fe0f82e649072302518f8570a5a8876b
Peacebern <peacebern@gmail.com>
2022-08-29 17:30:59 +0100

    class files

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+"""
+takes in the function of the base model
+"""
+
+from models.base_model import BaseModel
+class Amenity(BaseModel):
+    """
+    implements the amenity model
+    """
+    name= ""
